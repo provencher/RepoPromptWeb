3bd1c14657ba56cce8b4887d39cfd830680daa20
Eric Provencher <eprovencher92@gmail.com>
2024-09-24 15:39:27 -0400

    compiles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // File: src/App.js
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
 import Toolbar from './components/Toolbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Screenshots from './components/Screenshots';
-import './App.css';
-
-const { Footer } = Layout;
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -18,7 +16,7 @@ function App() {
       <Features />
       <Screenshots />
       <Footer style={{ textAlign: 'center' }}>
-        Repo Prompt ©2023 Created by YourName
+        Some Footer Content Here
       </Footer>
     </Layout>
   );
--- src/components/Features.js
+++ src/components/Features.js
@@ -16,7 +16,7 @@ function Features() {
       <Row gutter={[16, 16]}>
         {features.map((feature, index) => (
           <Col xs={24} sm={12} md={8} key={index}>
-            <Card title={feature.title}>
+            <Card title={feature.title} headStyle={{ color: '#FFFDD0' }}>
               <p>{feature.description}</p>
             </Card>
           </Col>
