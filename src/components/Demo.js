7ca0353e9fa725bb19c590a2d5de6bc39ff8f5d3
Eric Provencher <eprovencher92@gmail.com>
2024-09-26 14:19:26 -0400

    fixed video player

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,21 +1,24 @@
 // src/components/Demo.js
 
 import React from 'react';
+import ReactPlayer from 'react-player';
 import './Demo.css';
 
 function Demo() {
   return (
     <section id="demo" className="demo">
       <div className="demo-content">
-      <video
-        src="/videos/DarkModeSiteEdit.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-        loading="lazy"
-        className="demo-video"
-      />
+        <ReactPlayer
+          url="/videos/DarkModeSiteEdit.mp4"
+          playing={true}
+          loop={true}
+          muted={true}
+          playsinline={true}
+          controls={true}
+          width="100%"
+          height="auto"
+          className="demo-video"
+        />
       </div>
     </section>
   );
